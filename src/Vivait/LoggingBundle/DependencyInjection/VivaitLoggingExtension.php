e6f0ec5a38a77ebfc0348af14b8ebae3bbc02a16
leightonthomas <leighton.thomas@vivait.co.uk>
2017-02-23 10:04:32 +0000

    Review changes

--- src/Vivait/LoggingBundle/DependencyInjection/VivaitLoggingExtension.php
+++ src/Vivait/LoggingBundle/DependencyInjection/VivaitLoggingExtension.php
@@ -19,6 +19,6 @@ class VivaitLoggingExtension extends Extension
         $this->processConfiguration($configuration, $config);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('config.yml');
+        $loader->load('services.yml');
     }
 }
